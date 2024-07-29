86732637d37448612209afc3b97e36a63ae2e6c9
Selim <selim_hassani@hotmail.com>
2024-07-29 11:45:16 +0200

    initial commit preAUTH

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/app', name: 'app_')]
+
+class MainController extends AbstractController
+{
+    #[Route('/home', name: 'main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
